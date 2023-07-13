7f5867de51834a83b3af0c0da3ff63bb7849f46c
AilsaYang <ailsayang831031@gmail.com>
2023-07-13 19:07:46 +0800

    style: setting basic style format in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,87 @@
+import styled from "styled-components";
 
+//Styled Component
+
+const Container = styled.div`
+  border: 1px solid gray;
+  border-radius: 5px;
+  margin: 20px;
+  padding: 20px;
+  box-shadow: 3px 3px 10px 2px steelblue;
+`;
+const StyledH1 = styled.h1`
+  color: #23415a;
+  text-align: center;
+`;
+
+const StyledForm = styled.form`
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const StyledLabel = styled.label`
+  margin: 10px;
+  color: #23415a;
+  font-weight: 700;
+`;
+
+const StyledInput = styled.input`
+  width: 200px;
+  margin: 10px;
+  border-radius: 5px;
+  border: 1px solid steelblue;
+  padding: 10px;
+  outline: none;
+  &:focus {
+    border: 1px solid lightblue;
+    box-shadow: 1px 0 10px 0 lightseagreen;
+    transform: scale(1.1);
+  }
+`;
+const StyledButton = styled.button`
+  width: 100px;
+  height: 30px;
+  margin: 10px;
+  border: 1px solid steelblue;
+  background-color: steelblue;
+  color: white;
+  border-radius: 5px;
+
+  cursor: pointer;
+  &:hover {
+    box-shadow: 2px 0 10px 1px lightseagreen;
+    transform: scale(1.1);
+  }
+`;
+
+const StyledP = styled.p`
+  margin: 20px;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
+      <StyledH1>My First React Form</StyledH1>
 
-    </div>
+      <StyledForm>
+        <StyledLabel>Name:</StyledLabel>
+        <StyledInput className="name" type="text" placeholder="name" />
+
+        <br />
+
+        <StyledLabel>Email:</StyledLabel>
+        <StyledInput className="email" type="email" placeholder="email" />
+
+        <br />
+
+        <StyledButton className="button" type="submit">
+          Submit
+        </StyledButton>
+      </StyledForm>
+    </Container>
   );
 }
 
